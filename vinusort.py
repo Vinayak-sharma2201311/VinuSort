7e9309d989bac99c08fc407ea07441707d460fdd
Vinayak-sharma2201311 <sharmavinayak799@gmail.com>
2024-08-20 18:16:41 +0530

    3d commit

--- vinusort.py
+++ vinusort.py
@@ -1,6 +1,14 @@
 array = [2, 5, 7, 99, 11, 1010, 87, 554, 28472, 21, 1, 0]
 
 def vinusort(arr):
-    return 0
+    temp_array = []
+    for n in array:
+        if n > temp_array[len(array)-1]:
+            temp_array.append(n)
+        else:
+            temp_array[1]
+
+    for n in temp_array:
+        print(n)
 
 vinusort(array)
